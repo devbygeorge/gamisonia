a3354cc3123c9872cc2cce17919dbe293973c191
unknown <giopasieshvili2@gmail.com>
2023-06-28 16:45:17 +0400

    Remove i18n config from next.config.js

--- next.config.js
+++ next.config.js
@@ -4,11 +4,7 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ["cdn.sanity.io"],
-  },
-  i18n: {
-    locales: ["en", "ka"],
-    defaultLocale: "en",
-  },
+  }
 };
 
 module.exports = nextConfig;
