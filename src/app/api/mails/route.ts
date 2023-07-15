f4eda77d170df825413650ec85b2a9b79f4e81a6
Giorgi Pasieshvili <giopasieshvili2@gmail.com>
2023-07-15 22:13:02 +0400

    Build and use email template

--- src/app/api/mails/route.ts
+++ src/app/api/mails/route.ts
@@ -1,20 +1,33 @@
+import fs from "fs";
+import handlebars from "handlebars";
 import { NextRequest } from "next/server";
 import nodemailer from "nodemailer";
+import { promisify } from "util";
+
+const readFile = promisify(fs.readFile);
 
 export async function POST(req: NextRequest) {
   const { name, email, phone, message } = await req.json();
 
   try {
+    let html = await readFile("src/views/email.handlebars", "utf8");
+    let template = handlebars.compile(html);
+    let data = {
+      name,
+      email,
+      phone,
+      message,
+    };
+    let htmlToSend = template(data);
+
     await transporter.sendMail({
       ...mailOptions,
       subject: "New email from gamisonia.com",
-      html: ` <p><strong>Full Name:</strong> ${name}</p>
-                <p><strong>Email Address:</strong> ${email}</p>
-                <p><strong>Phone Number:</strong> ${phone}</p>
-                <p><strong>Message:</strong> ${message}</p>`,
+      html: htmlToSend,
     });
     return new Response("Mail Sent", { status: 201 });
   } catch (error) {
+    console.log(error);
     return new Response("Failed to send mail", { status: 500 });
   }
 }
